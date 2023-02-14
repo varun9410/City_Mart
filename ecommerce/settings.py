48ebcbbab30f89251ef92ccb41df8136a616e85a
varun from ubuntu <varun941068@gmail.com>
2023-02-14 10:14:05 -0800

    change thedatabse to sqlit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -78,14 +78,14 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''
-DATABASES = {
+
+'''DATABASES = {
    'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ecommerce',
@@ -94,8 +94,7 @@ DATABASES = {
         'HOST':'localhost',
         'PORT':'3306',
     }
-}
-
+}'''
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
